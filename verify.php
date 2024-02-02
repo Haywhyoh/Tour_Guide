c6f7940a2e8593422f055ec6f4a83431a4fc6c74
Ayomide <adefeyisayo998@gmail.com>
2024-02-02 06:21:49 +0100

    fixed a typo

--- verify.php
+++ verify.php
@@ -8,19 +8,15 @@ $demo_user = [
     'email' => 'test@gmail.com',
     'firstName' => 'Test',
     'lastName' => 'User',
-    'role' => 'user'
+    'role' => 'user',
 ];
 
 $user = new User();
-
 $user->saveToDatabase($demo_user);
-
 // $user->__set('username', $demo_user['username']);
 // $user->__set('passwordHash', $demo_user['password']);
 // $user->__set('email', $demo_user['email']);
 // $user->__set('firstName', $demo_user['firstName']);
 // $user->__set('lastName', $demo_user['lastName']);
 // $user->__set('role', $demo_user['role']);
-
-
 ?>
